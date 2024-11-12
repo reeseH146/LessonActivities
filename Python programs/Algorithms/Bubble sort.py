8c843c986492e7b94776bcf612fbb2f48ed99bf7
Reese <reesehe.66146@gmail.com>
2024-11-12 15:16:28 +0000

    Fixed EfficBubbleSort and tested it with some large values which did not make my laptop too happy.

--- Python programs/Algorithms/Bubble sort.py
+++ Python programs/Algorithms/Bubble sort.py
@@ -1,7 +1,7 @@
 # This is a bubble sort
 import random as r
 
-UnsortedList = [x for x in range(0, 250000)]
+UnsortedList = [r.randint(0, 100) for x in range(250)]
 
 def BubbleSort(List):
     # Loops through the list multiple times
@@ -18,11 +18,11 @@ def BubbleSort(List):
 def EfficBubbleSort(List):
     # Loops through the list multiple times
     Sorted = True
-    n = len(List) - 1
+    n = len(List)
     while Sorted and n > 0:
+        n -= 1
         # Bubbles up the list again
         for y in range(n):
-            n -= 1
             # Swaps the element and the next element if not sorted
             if List[y] > List[y + 1]:
                 Temp = List[y]
@@ -31,6 +31,11 @@ def EfficBubbleSort(List):
                 Sorted = True
     return List
 
-print(UnsortedList)
-print(BubbleSort(UnsortedList))
-#print(EfficBubbleSort(UnsortedList))
+#print(UnsortedList)
+#print(BubbleSort(UnsortedList))
+CheckList = EfficBubbleSort(UnsortedList)
+print(CheckList)
+for x in range(len(CheckList) - 1):
+    if CheckList[x] > CheckList[x + 1]:
+        print("FAIL!!!", x, CheckList[x], CheckList[x + 1])
+        break
