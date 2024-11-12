6b606fed9b73542eecd828ce5d14a51c2f283e70
Reese <reesehe.66146@gmail.com>
2024-11-12 14:18:06 +0000

    Improved bubble sort and added insertion sort, managed to do it quickly.

--- Python programs/Algorithms/Bubble sort.py
+++ Python programs/Algorithms/Bubble sort.py
@@ -1,13 +1,13 @@
 # This is a bubble sort
 import random as r
 
-UnsortedList = [r.randint(0, 10) for x in range(19)]
+UnsortedList = [x for x in range(0, 250000)]
 
 def BubbleSort(List):
     # Loops through the list multiple times
     for x in range(len(List) - 1):
         # Bubbles up the list again
-        for y in range(len(List) - 1):
+        for y in range((len(List) - 1)-x):
             # Swaps the element and the next element if not sorted
             if List[y] > List[y + 1]:
                 Temp = List[y]
@@ -15,6 +15,22 @@ def BubbleSort(List):
                 List[y+1] = Temp
     return List
 
+def EfficBubbleSort(List):
+    # Loops through the list multiple times
+    Sorted = True
+    n = len(List) - 1
+    while Sorted and n > 0:
+        # Bubbles up the list again
+        for y in range(n):
+            n -= 1
+            # Swaps the element and the next element if not sorted
+            if List[y] > List[y + 1]:
+                Temp = List[y]
+                List[y] = List[y + 1]
+                List[y+1] = Temp
+                Sorted = True
+    return List
 
-
+print(UnsortedList)
 print(BubbleSort(UnsortedList))
+#print(EfficBubbleSort(UnsortedList))
---
+++ Python programs/Algorithms/Insertion sort.py
@@ -0,0 +1,20 @@
+import random as r
+
+UnsortedList = [r.randint(0, 10) for x in range(0, 10)]
+
+def InsertionSort(List):
+    for x in range(1, len(List)):
+        num = x
+        while List[num - 1] > List[num]:
+            Temp = List[num]
+            List[num] = List[num - 1]
+            List[num - 1] = Temp
+            num -= 1
+            if num == 0:
+                break
+
+    return List
+
+
+print(UnsortedList)
+print(InsertionSort(UnsortedList))
