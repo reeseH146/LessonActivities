419e91b7833db16e944b800983143168cc2958eb
Reese <reesehe.66146@gmail.com>
2025-02-24 12:22:29 +0000

    Commented code for lesson activity.

--- Python programs/Basic programming structures/PP2-Q7A&B.py
+++ Python programs/Basic programming structures/PP2-Q7A&B.py
@@ -1,18 +1,21 @@
+# Function to convert denary number to binary number with unpadded 0
 def toBinary(val):
-    temp = val
-    bin = ""
-    while temp > 0:
-        if temp % 2 == 0:
+    bin = "" # Stores the result of binary value converison
+    while val > 0: # Loops through until denary number is greater than 0
+        if val % 2 == 0: # If MOD 2 returns no remainders then adds 0 to the start of bin string since the output is given in reverse order
             bin = "0" + bin
-        elif temp % 2 == 1:
+        elif val % 2 == 1: # If MOD 2 returns remainder of 1 then adds 1
             bin = "1" + bin
-        temp = temp // 2
+        val = val // 2 # Halves the number rounded down, each place value in a binary number is double the previous
     return bin
 
-userInput = -1
-while (1 > userInput) or (userInput > 255):
-    userInput = int(input("Enter a num to convert to a binary value : "))
+# User input and validation for denary numbers to convert to binary
+userInput = -1 # Default case to promote loop to run
+while (1 > userInput) or (userInput > 255): # Denary number has to be integer between 1 and 255, otherwise continue asking for user input
+    userInput = int(input("Enter a num to convert to a binary value : ")) # Asks for a integer denary number
+# Prints result of converting denary number input to binary number from "toBinary" function
 print(toBinary(userInput))
 
+# Commented out code which tested the toBinary function with numbers ranging from 1 to 255
 """for i in range(1, 256):
     print(toBinary(i))"""
