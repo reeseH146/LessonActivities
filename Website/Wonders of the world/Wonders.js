177179d362c39bffa3ddd22291f94d1a70208d3e
Reese <reesehe.66146@gmail.com>
2025-03-11 11:07:03 +0000

    Made further changes.

--- Website/Wonders of the world/Wonders.js
+++ Website/Wonders of the world/Wonders.js
@@ -1,1 +1,12 @@
 // alert("help")
+
+/* function FormGetCurrentDate() {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    const CurrentDate = `${year}-${month}-${day}`;
+    document.getElementById("DateOfVisit").setAttribute("max", CurrentDate);
+    alert(CurrentDate);
+} 
+*/
