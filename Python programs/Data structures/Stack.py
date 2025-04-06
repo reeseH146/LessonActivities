6e17183f28a6ccb3f648782c497747dcc62125d3
Reese <reesehe.66146@gmail.com>
2025-04-06 01:18:24 +0100

    Stack was missing code I wrote?

--- Python programs/Data structures/Stack.py
+++ Python programs/Data structures/Stack.py
@@ -1,25 +1,22 @@
-class Queue:
+class Stack:
     def __init__(self, Length):
-        self.Queue = [None for x in range(Length)]
-        self.LPointer = -1
+        self.Stack = [None for x in range(Length)]
         self.HPointer = -1
         self.MaxPointer = len(self.Stack) - 1
     
-    #...
     def Peak(self):
         if self.Stack[self.HPointer]:
             print(f"{self.Stack[self.HPointer]} at index {self.HPointer}")
         else:
             print("ERROR : Stack empty")
 
-    #...
     def Pop(self):
-        if 0 < self.HPointer:
+        if 0 <= self.HPointer:
             self.Stack[self.HPointer] = None
             self.HPointer -= 1
         else:
             print("ERROR : Reached EOS") # End Of Stack
-    #...
+
     def Push(self, PushItem):
         if self.HPointer < self.MaxPointer:
             self.HPointer += 1
@@ -28,16 +25,18 @@ class Queue:
             print("ERROR : Stack full")
 
 End = False
-QueueExample = Queue(5)
+StackExample = Stack(5)
 while not End:
-    print("\n", QueueExample.Queue)
+    print(StackExample.Stack)
     Method = input("What method to do to the stack? : ")
     if Method == "Peak":
-        QueueExample #...
+        StackExample.Peak()
     elif Method == "Pop":
-        QueueExample #...
+        StackExample.Pop()
     elif Method == "Push":
         PushItem = input("Push Item : ")
-        QueueExample #...
+        StackExample.Push(PushItem)
+    elif Method == "Quit":
+        raise SystemExit
     else:
         print("ERROR : Not valid command")
